57cb9047beb3560a87187cd5830960a532bb55c6
Sam Knight <sam.knight@boxuk.com>
2013-06-27 11:01:18 +0100

    Add parent association between Place and Organisation

--- lib/carbonculture/place.rb
+++ lib/carbonculture/place.rb
@@ -1,15 +1,20 @@
 module Carbonculture
   class Place
     include HTTParty
-    attr_accessor :name, :data, :body
+    attr_accessor :name, :organisation_name, :data, :body
 
     def initialize(name, org_name)
       self.name = name
+      self.organisation_name = org_name
       self.data = self.class.get "#{ BASE_URL }/#{ org_name }/#{ name }"
 
       self.body = JSON.parse(self.data.body)
       raise ArgumentError if status == 'fail'
 
+    end
+
+    def organisation
+      Organisation.new(organisation_name)
     end
 
     def method_missing(method_name, *args, &block)
--- spec/lib/carbonculture/place_spec.rb
+++ spec/lib/carbonculture/place_spec.rb
@@ -13,8 +13,8 @@ describe Carbonculture::Place do
   describe 'initialize class' do
 
     it 'must contain accept valid Place' do
-      org = Carbonculture::Place.new('10-downing-street', 'number10')
-      org.data.response.code.must_equal '200'
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      place.data.response.code.must_equal '200'
     end
 
     it 'must raise an error with an invalid Place' do
@@ -22,8 +22,8 @@ describe Carbonculture::Place do
     end
 
     it 'parses the API response from JSON to a hash' do
-      org = Carbonculture::Place.new('10-downing-street', 'number10')
-      org.data.must_be_instance_of Hash
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      place.data.must_be_instance_of Hash
     end
 
   end
@@ -31,13 +31,27 @@ describe Carbonculture::Place do
   describe 'dynamic attributes' do
 
     it 'returns the attribute value if present in the profile' do
-      org = Carbonculture::Place.new('10-downing-street', 'number10')
-      org.title.must_equal '10 Downing Street'
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      place.title.must_equal '10 Downing Street'
     end
 
     it 'raises a NoMethodError if an attribute is not present' do
-      org = Carbonculture::Place.new('10-downing-street', 'number10')
-      lambda { org.wrong }.must_raise NoMethodError
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      lambda { place.wrong }.must_raise NoMethodError
+    end
+
+  end
+
+  describe 'Organisation association' do
+
+    it 'returns an Organisation' do
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      place.organisation.must_be_kind_of Carbonculture::Organisation
+    end
+
+    it 'returns an Array of Places' do
+      place = Carbonculture::Place.new('10-downing-street', 'number10')
+      place.organisation.name.must_equal 'number10'
     end
 
   end
